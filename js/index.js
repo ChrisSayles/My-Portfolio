40874f5f14f1d77b70f612dd452f37f94617d27f
Chris Sayles <chrissayles@yahoo.com>
2017-05-05 14:47:54 -0500

    fixed image slider issue

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@
 //         tempTypeSpeed = 0;
 
 //         var type = function() {
-        
+
 //             if (writingTag === true) {
 //                 tag += HTML[cursorPosition];
 //             }
@@ -71,37 +71,21 @@
 
      //CAROUSEL
 
-       document.addEventListener("DOMContentLoaded", function() {
-
-    var rotateComplete = function() {
-      target.style.webkitAnimationName = "";
-      target.insertBefore(arr[arr.length-1], arr[0]);
-      setTimeout(function(el) {
-        el.style.webkitAnimationName = "rotator";
-      }, 0, target);
-    };
-
-    var target = document.getElementById("rotator");
-    var arr = target.getElementsByTagName("a");
-
-    target.addEventListener("webkitAnimationEnd", rotateComplete, false);
-
-  }, false);
-        var rotateComplete = function() {
-    with(target.style) {
-      webkitAnimationName = MozAnimationName = msAnimationName = "";
-    }
-    target.insertBefore(arr[arr.length-1], arr[0]);
-    setTimeout(function(el) {
-      with(el.style) {
-        webkitAnimationName = MozAnimationName = msAnimationName = "rotator";
-      }
-    }, 0, target);
-  };
-
-  var target = document.getElementById("rotator");
-  var arr = target.getElementsByTagName("a");
-
-  target.addEventListener("webkitAnimationEnd", rotateComplete, false);
-  target.addEventListener("animationend", rotateComplete, false);
-  target.addEventListener("MSAnimationEnd", rotateComplete, false);
+    //  var rotateComplete = function() {
+    //      with(target.style) {
+    //        webkitAnimationName = MozAnimationName = msAnimationName = "";
+    //      }
+    //      target.insertBefore(arr[arr.length-1], arr[0]);
+    //      setTimeout(function(el) {
+    //        with(el.style) {
+    //          webkitAnimationName = MozAnimationName = msAnimationName = "rotator";
+    //        }
+    //      }, 0, target);
+    //    };
+     //
+    //    var target = document.getElementById("rotator");
+    //    var arr = target.getElementsByTagName("a");
+     //
+    //    target.addEventListener("webkitAnimationEnd", rotateComplete, false);
+    //    target.addEventListener("animationend", rotateComplete, false);
+    //    target.addEventListener("MSAnimationEnd", rotateComplete, false);
--- public/js/index.js
+++ public/js/index.js
@@ -71,37 +71,21 @@
 
      //CAROUSEL
 
-       document.addEventListener("DOMContentLoaded", function() {
+     var rotateComplete = function() {
+         with(target.style) {
+           webkitAnimationName = MozAnimationName = msAnimationName = "";
+         }
+         target.insertBefore(arr[arr.length-1], arr[0]);
+         setTimeout(function(el) {
+           with(el.style) {
+             webkitAnimationName = MozAnimationName = msAnimationName = "rotator";
+           }
+         }, 0, target);
+       };
 
-    var rotateComplete = function() {
-      target.style.webkitAnimationName = "";
-      target.insertBefore(arr[arr.length-1], arr[0]);
-      setTimeout(function(el) {
-        el.style.webkitAnimationName = "rotator";
-      }, 0, target);
-    };
+       var target = document.getElementById("rotator");
+       var arr = target.getElementsByTagName("a");
 
-    var target = document.getElementById("rotator");
-    var arr = target.getElementsByTagName("a");
-
-    target.addEventListener("webkitAnimationEnd", rotateComplete, false);
-
-  }, false);
-        var rotateComplete = function() {
-    with(target.style) {
-      webkitAnimationName = MozAnimationName = msAnimationName = "";
-    }
-    target.insertBefore(arr[arr.length-1], arr[0]);
-    setTimeout(function(el) {
-      with(el.style) {
-        webkitAnimationName = MozAnimationName = msAnimationName = "rotator";
-      }
-    }, 0, target);
-  };
-
-  var target = document.getElementById("rotator");
-  var arr = target.getElementsByTagName("a");
-
-  target.addEventListener("webkitAnimationEnd", rotateComplete, false);
-  target.addEventListener("animationend", rotateComplete, false);
-  target.addEventListener("MSAnimationEnd", rotateComplete, false);
+       target.addEventListener("webkitAnimationEnd", rotateComplete, false);
+       target.addEventListener("animationend", rotateComplete, false);
+       target.addEventListener("MSAnimationEnd", rotateComplete, false);
